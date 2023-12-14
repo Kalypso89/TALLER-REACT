150f80731a96c2466a5f442dc7a25c38f3ec9ed4
Angela <antunezangela89@gmail.com>
2023-12-15 00:54:15 +0100

    Import AboutView component in Router component

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeView from '../components/views/HomeView';
 import { CollectionView } from '../components/views/CollectionView';
+import {AboutView} from '../components/views/AboutView';
+
 const Router = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<HomeView/>}></Route>
             <Route path='/collection' element={<CollectionView/>}></Route>
+            <Route path='/about' element={<AboutView/>}></Route>
         </Routes>
     </BrowserRouter>
   );
