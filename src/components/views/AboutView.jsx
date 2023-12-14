fbb057462e5f91052bcb57a1ba49ff20970a80ec
Angela <antunezangela89@gmail.com>
2023-12-15 00:48:53 +0100

    Create AboutView component

---
+++ src/components/views/AboutView.jsx
@@ -0,0 +1,10 @@
+import styles from "./aboutView.module.css"
+
+export function AboutView(){
+    return(
+        <>
+            <h1 className={styles.title}>About Me</h1>
+            <p className={styles.p1}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro minima provident, atque, doloribus fugit quidem a sapiente recusandae, inventore nihil ea repellat quis molestiae laboriosam tenetur reiciendis. Sint, eum modi?</p>
+        </>
+    );
+}
